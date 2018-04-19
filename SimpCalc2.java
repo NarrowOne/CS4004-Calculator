8f3f578e1f9b0e27af71dc37fffc96d69036d9df
Caolan O'Mara Kilbane <comkilbane@gmail.com>
2018-04-19 12:35:44 +0100

    Program v2

---
+++ SimpCalc2.java
@@ -0,0 +1,14 @@
+package ie.Calc;
+
+public class SimpCalc {
+	public SimpCalc() {
+		
+	}
+	public int add(int x, int y) {
+		return x+y;
+	}
+	public int sub(int x, int y) {
+		return x-y;
+	}
+
+}
